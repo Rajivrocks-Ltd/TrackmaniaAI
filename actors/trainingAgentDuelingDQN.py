3d9d9a5a7a1243089170a341ac1975f39eb8283e
kacperkadziolka <k.r.kadziolka@gmail.com>
2024-05-13 23:55:48 +0200

    evan little meltz v2

--- actors/trainingAgentDuelingDQN.py
+++ actors/trainingAgentDuelingDQN.py
@@ -33,62 +33,41 @@ class DuelingDQNTrainingAgent(TrainingAgent):
                          action_space=action_space,
                          device=device)
 
-
         model = model_cls(observation_space, action_space)
         self.model = model.to(self.device)
         self.target_model = no_grad(deepcopy(self.model))
         self.target_model.eval()
         self.lr = lr
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
-        self.memory = deque(maxlen=memory_size)
         self.batch_size = batch_size
         self.gamma = gamma
         self.update_buffer_interval = update_buffer_interval
         self.steps_done = 0
 
     def get_actor(self):
-        return self.model_nograd.actor
+        return self.model
 
-    def update(self):
+    def update(self, batch):
 
-        batch = random.sample(self.memory, self.batch_size)
         o, a, r, o2, d, _ = batch
 
-        'Create from numpy arrays, torch tensor and make them 2D to work with batches'
-        # Convert to tensors
-        states = torch.FloatTensor(np.float32(o)).to(self.device)
-        actions = torch.LongTensor(a).unsqueeze(1).to(self.device)
-        rewards = torch.FloatTensor(r).unsqueeze(1).to(self.device)
-        next_states = torch.FloatTensor(np.float32(o2)).to(self.device)
-        dones = torch.FloatTensor(d).unsqueeze(1).to(self.device)
+        pi_action, logp_pi = self.model(o)
 
-        # Compute current Q values: Q(s, a)
-        current_q_values = self.model(states).gather(1, actions)
+        policy_loss = -(logp_pi * r).mean()
 
-        # Compute expected Q values from target actions: max_a' Q'(s', a')
-        next_q_values = self.target_model(next_states).max(1)[0].detach()
-        expected_q_values = rewards + self.gamma * next_q_values * (1 - dones)
-
-        # Compute loss
-        loss = F.mse_loss(current_q_values.squeeze(), expected_q_values)
-
-        # Gradient descent update
+        # Gradient descent steps to update the model parameters
         self.optimizer.zero_grad()
-        loss.backward()
+        policy_loss.backward()
         self.optimizer.step()
 
-        return loss
-
-    def update_memory(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+        return policy_loss
 
     def train(self, batch):
 
         # First, we decompose our batch into its relevant components, ignoring the "truncated" signal:
         o, a, r, o2, d, _ = batch
 
-        self.update_memory(o, a, r, o2, d)
-        loss = self.update()
+        loss = self.update(batch)
 
         # Optionally update the target network
         if self.steps_done % self.update_buffer_interval == 0:
